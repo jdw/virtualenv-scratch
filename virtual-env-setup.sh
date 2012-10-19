5cf512443dbabf95618a823364fa3ec24d8b847c
Johannes D. Wirde <johannes.wirde@gmail.com>
2012-10-19 21:59:41 +0200

    Rearranging INIT support and adding helper output.

--- virtual-env-setup.sh
+++ virtual-env-setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if [ $@ == "INIT" ]; then
+INIT="nope"
+if [ $# -eq 1 ]; then
+	if [ $@ == "INIT" ]; then
+		INIT="yes"
+	fi
+fi
+
+if [ $INIT = "yes" ]; then
 	echo "Initializing virtual environment..."
 	virtualenv .
 fi
 
-source bin/activate
+. bin/activate
 
-if [ $@ == "INIT" ]; then
+if [ $INIT = "yes" ]; then
 	echo "Installing packages..."
 	easy_install pygame
 	easy_install flask
 fi
+
+echo "Now run '. bin/activate'"
