1fe5f9f9006a1d71bb20bbc6b5fd272ba1498cd4
Johannes D. Wirde <johannes.wirde@gmail.com>
2012-10-19 21:16:36 +0200

    Adding "INIT" param.

--- virtual-env-setup.sh
+++ virtual-env-setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-virtualenv .
+
+if [ $@ == "INIT" ]; then
+	echo "Initializing virtual environment..."
+	virtualenv .
+fi
+
 source bin/activate
+
+if [ $@ == "INIT" ]; then
+	echo "Installing packages..."
+	easy_install pygame
+	easy_install flask
+fi
